95f6301aab0b963fdb8ff664f11c75d10b3ebdd4
Kyle Nguyen <nghia002@outlook.com>
2025-09-20 00:38:07 +0800

    P1 - mtgf client - first try

--- pr1/mtgf/gfclient_download.c
+++ pr1/mtgf/gfclient_download.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "gfclient-student.h"
+#include "workload.h"
 
 #define MAX_THREADS 1024
 #define PATH_BUFFER_SIZE 512
@@ -25,6 +27,12 @@ static struct option gLongOptions[] = {
     {"workload", required_argument, NULL, 'w'},
     {"nrequests", required_argument, NULL, 'n'},
     {NULL, 0, NULL, 0}};
+  
+typedef struct {
+  char* server;
+  unsigned short port;
+  int n_requests;
+} worker_args_t;
 
 static void Usage() { fprintf(stderr, "%s", USAGE); }
 
@@ -68,6 +76,81 @@ static void writecb(void *data, size_t data_len, void *arg) {
   fwrite(data, 1, data_len, file);
 }
 
+static unsigned short int counter = 0;
+static pthread_mutex_t counter_mutex;
+
+int download(char* server, int port, char* req_path) {
+  gfcrequest_t *gfr = NULL;
+  char local_path[PATH_BUFFER_SIZE];
+  int returncode = 0;
+  localPath(req_path, local_path);
+
+  FILE *file = openFile(local_path);
+
+  gfr = gfc_create();
+  gfc_set_path(&gfr, req_path);
+
+  gfc_set_port(&gfr, port);
+  gfc_set_server(&gfr, server);
+  gfc_set_writearg(&gfr, file);
+  gfc_set_writefunc(&gfr, writecb);
+
+  fprintf(stdout, "Requesting %s%s\n", server, req_path);
+
+  if (0 > (returncode = gfc_perform(&gfr))) {
+    fprintf(stdout, "gfc_perform returned an error %d\n", returncode);
+    fclose(file);
+    if (0 > unlink(local_path))
+      fprintf(stderr, "warning: unlink failed on %s\n", local_path);
+  } else {
+    fclose(file);
+  }
+
+  if (gfc_get_status(&gfr) != GF_OK) {
+    if (0 > unlink(local_path)) {
+      fprintf(stderr, "warning: unlink failed on %s\n", local_path);
+    }
+  }
+
+  fprintf(stdout, "Status: %s\n", gfc_strstatus(gfc_get_status(&gfr)));
+  fprintf(stdout, "Received %zu of %zu bytes\n", gfc_get_bytesreceived(&gfr),
+          gfc_get_filelen(&gfr));
+
+  gfc_cleanup(&gfr);
+  return 0;
+}
+
+void* download_thread(void* args) {
+  worker_args_t* worker_args = (worker_args_t*)args; // constant throughout the thread's life
+  char* req_path;
+  while (1) {
+    // acquire mutex here to check for end of workload and get next req_path
+    req_path = NULL;
+    int request_id = -1;
+    pthread_mutex_lock(&counter_mutex);
+    if (counter < worker_args->n_requests) {
+      req_path =  workload_get_path();
+      request_id = counter++;
+    }
+    pthread_mutex_unlock(&counter_mutex);
+    
+    if (req_path != NULL){
+      if (strlen(req_path) > PATH_BUFFER_SIZE) {
+        fprintf(stderr, "Request path exceeded maximum of %d characters\n.", PATH_BUFFER_SIZE);
+        continue; // TODO: maybe not exit in the thread, but need a way to tell the main thread that the download failed
+      }
+
+      if (0 > download(worker_args->server, worker_args->port, req_path)) {
+        fprintf(stderr, "Error downloading file %s\n", req_path);
+      } else {
+        printf("Request %d complete\n", request_id);
+      }
+    } else {
+      return NULL;
+    }
+  }
+}
+
 /* Main ========================================================= */
 int main(int argc, char **argv) {
   /* COMMAND LINE OPTIONS ============================================= */
@@ -77,13 +160,7 @@ int main(int argc, char **argv) {
   int option_char = 0;
   int nthreads = 8;
 
-  int returncode = 0;
-  char *req_path = NULL;
-  char local_path[PATH_BUFFER_SIZE];
   int nrequests = 14;
-
-  gfcrequest_t *gfr = NULL;
-  FILE *file = NULL;
 
   setbuf(stdout, NULL);  // disable caching
 
@@ -133,61 +210,30 @@ int main(int argc, char **argv) {
   }
   gfc_global_init();
 
+  // initialize results
+  pthread_mutex_init(&counter_mutex, NULL);
+
   // add your threadpool creation here
-
-  /* Build your queue of requests here */
-  for (int i = 0; i < nrequests; i++) {
-    /* Note that when you have a worker thread pool, you will need to move this
-     * logic into the worker threads */
-    req_path = workload_get_path();
-
-    if (strlen(req_path) > PATH_BUFFER_SIZE) {
-      fprintf(stderr, "Request path exceeded maximum of %d characters\n.", PATH_BUFFER_SIZE);
-      exit(EXIT_FAILURE);
-    }
-
-    localPath(req_path, local_path);
-
-    file = openFile(local_path);
-
-    gfr = gfc_create();
-    gfc_set_path(&gfr, req_path);
-
-    gfc_set_port(&gfr, port);
-    gfc_set_server(&gfr, server);
-    gfc_set_writearg(&gfr, file);
-    gfc_set_writefunc(&gfr, writecb);
-
-
-
-    fprintf(stdout, "Requesting %s%s\n", server, req_path);
-
-    if (0 > (returncode = gfc_perform(&gfr))) {
-      fprintf(stdout, "gfc_perform returned an error %d\n", returncode);
-      fclose(file);
-      if (0 > unlink(local_path))
-        fprintf(stderr, "warning: unlink failed on %s\n", local_path);
-    } else {
-      fclose(file);
-    }
-
-    if (gfc_get_status(&gfr) != GF_OK) {
-      if (0 > unlink(local_path)) {
-        fprintf(stderr, "warning: unlink failed on %s\n", local_path);
-      }
-    }
-
-    fprintf(stdout, "Status: %s\n", gfc_strstatus(gfc_get_status(&gfr)));
-    fprintf(stdout, "Received %zu of %zu bytes\n", gfc_get_bytesreceived(&gfr),
-            gfc_get_filelen(&gfr));
-
-    gfc_cleanup(&gfr);
-
-    /*
-     * note that when you move the above logic into your worker thread, you will
-     * need to coordinate with the boss thread here to effect a clean shutdown.
-     */
+  pthread_t* thread_pool = (pthread_t*)malloc(nthreads * sizeof(pthread_t));
+  worker_args_t args;
+  args.server = server;
+  args.port = port;
+  args.n_requests = nrequests; // read-only args so can share
+  for (int i=0; i<nthreads; i++) {
+    pthread_create(&thread_pool[i], NULL, download_thread, &args);
   }
+  for (int i=0; i<nthreads; i++) {
+    pthread_join(thread_pool[i], NULL);
+  }
+  // TODO: handle workers go rogue
+  printf("Doanloaded all files.\n");
+  // clean up
+  free(thread_pool);
+
+  /*
+   * note that when you move the above logic into your worker thread, you will
+   * need to coordinate with the boss thread here to effect a clean shutdown.
+   */
 
   gfc_global_cleanup();  /* use for any global cleanup for AFTER your thread
                           pool has terminated. */
